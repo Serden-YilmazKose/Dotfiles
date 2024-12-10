3e08ba6b6d1f63e15916e0f24b6fad2dd89b0323
Serden-YilmazKose <Serden-Yilmaz.Kose@student.oulu.fi>
2024-12-10 02:47:36 +0200

    fixed shell script

--- dotfiles_script.sh
+++ dotfiles_script.sh
@@ -1,3 +1,3 @@
-cp i3/config ~/.i3/
+cp i3/config ~/.config/i3/
 cp i3/i3status/config ~/.config/i3status/
-cp neofetch/config.conf ~/.config/neofetch/config.conf
+cp neofetch/config.conf ~/.config/neofetch/
