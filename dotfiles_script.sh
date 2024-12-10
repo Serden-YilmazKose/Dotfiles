3cc630ca42105fa7e278e688cb121bbcf5c02171
Serden-YilmazKose <Serden-Yilmaz.Kose@student.oulu.fi>
2024-12-10 02:16:14 +0200

    Added neofetch script.

--- dotfiles_script.sh
+++ dotfiles_script.sh
@@ -1,2 +1,3 @@
-mv i3/config ~/.i3/
-mv i3/i3status/config ~/.config/i3status/
+cp i3/config ~/.i3/
+cp i3/i3status/config ~/.config/i3status/
+cp neofetch/config.conf ~/.config/neofetch/config.conf
