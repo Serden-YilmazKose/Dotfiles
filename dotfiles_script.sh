26d41de4d53a67535caf2357ab89d82931be6b8f
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-05-03 01:05:45 +0300

    Updated the script

--- dotfiles_script.sh
+++ dotfiles_script.sh
@@ -1,3 +1,6 @@
 cp i3/config ~/.config/i3/
 cp i3/i3status/config ~/.config/i3status/
 cp neofetch/config.conf ~/.config/neofetch/
+cp nvim/init.vim ~/.config/nvim/init.vim
+cp zsh/.zshr ~/.zshrc
+cp p10l/.p10k.zsh ~/.p10k.zsh
