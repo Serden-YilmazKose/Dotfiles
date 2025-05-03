f5b59bd33baddbd5b4cd29d0de8d9a4c3f0c29cd
Serden-YilmazKose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-05-03 13:07:36 +0300

    Updated dotfiles file

--- dotfiles_script.sh
+++ dotfiles_script.sh
@@ -1,8 +1,8 @@
 #/bin/zsh
 # UNCOMMENT THE FILES YOU WANT TO BE AFFECTED
-cp i3/config ~/.config/i3/
-cp i3/i3status/config ~/.config/i3status/
-cp neofetch/config.conf ~/.config/neofetch/
+cp i3/config ~/.config/i3/config
+cp i3/i3status/config ~/.config/i3status/config
+cp neofetch/config.conf ~/.config/neofetch/config.conf
 cp nvim/init.vim ~/.config/nvim/init.vim
 cp zsh/.zshrc ~/.zshrc
 # cp p10k/.p10k.zsh ~/.p10k.zsh
