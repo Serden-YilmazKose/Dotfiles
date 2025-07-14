170eb73dc885b171d38ddb7797e4294083901f5f
Serden-YilmazKose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-07-14 10:11:23 -0500

    Updated video.sh

--- i3/video.sh
+++ i3/video.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Open dmenu, and show the list of videos and ask the user to select on
 video=""
-cd ~/Videos/eighty/movies || exit 1
+cd ~/Videos/eighty || exit 1
 while true; do
     video="$(ls | dmenu -l 15 -p "Select video to watch: ")"
     [[ -z "$video" ]] && break
     [[ -d "$video" ]]; cd "$video"
-    [[ -f "$video" ]]; mpv "$video" && break
+    [[ -f "$video" ]]; vlc "$video" && break
 done
