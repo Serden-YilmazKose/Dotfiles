b80ecf7671b2c49987d7054dcd55b55180d2757f
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-07-14 22:21:39 -0500

    Video dmenu scrip works smoothly!

--- i3/video.sh
+++ i3/video.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Open dmenu, and show the list of videos and ask the user to select on
 video=""
-cd ~/Videos/|| exit 1
-while true; do
+cd ~/Videos/ || exit 1
+while [[ ! -f $video ]] ; do
     video="$(ls | dmenu -l 15 -p "Select video to watch: ")"
     [[ -z "$video" ]] && break
-    [[ -d "$video" ]]; cd "$video"
-    [[ -f "$video" ]]; vlc "$video" && break
+    [[ -d "$video" ]]; cd "$video" && continue
+    [[ -f "$video" ]]; mpv "$video" & break
 done
