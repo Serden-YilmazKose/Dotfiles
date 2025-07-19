9ff5aee21b399d7584e785120555552154b854c2
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-07-18 20:36:08 -0500

    Added bookmark saving script

---
+++ i3/save_bookmark.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+bookmark="$(xclip -o)"
+file="$HOME/.i3/bookmarks.txt"
+
+if grep -Fxq "$bookmark" "$file"; then
+    notify-send "Oops." "Already bookmarked!"
+    exit 0
+fi
+
+notify-send "Bookmark added!" "$bookmark is now saved to the file."
+echo "$bookmark" >> "$file"
