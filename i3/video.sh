d2d1f7d1fe2f928c7b1d5b622bf62922daf61063
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-07-28 23:06:58 -0500

    Added pictures shell script and improved video script

--- i3/video.sh
+++ i3/video.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Open dmenu, and show the list of videos and ask the user to select on
 video=""
+player="vlc"
 cd ~/Videos/ || exit 1
 while [[ ! -f $video ]] ; do
     video="$(ls | dmenu -l 15 -p "Select video to watch: ")"
     [[ -z "$video" ]] && break
+    [[ ! -d "$video" ]] || [[ ! -f "$video" ]] && continue
+    [[ -d "$video" ]] && [[ "$video" == "tmp" ]] && player="mpv"
     [[ -d "$video" ]]; cd "$video" && continue
-    [[ -f "$video" ]]; vlc "$video" && break
+    [[ -f "$video" ]]; $player "$video" && break
 done
 
 input=$(dmenu -p "Delete video after viewing [YES]: ")
