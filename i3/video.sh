39a8c16e5a0dbcb63db6795dc2237d523ba082ad
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-07-31 10:43:54 -0500

    Added boolOpened logic in video script

--- i3/video.sh
+++ i3/video.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Open dmenu, and show the list of videos and ask the user to select on
 video=""
-player="vlc"
+player="mpv"
+boolOpened=$false
 cd ~/Videos/ || exit 1
 while [[ ! -f $video ]] ; do
     video="$(ls | dmenu -l 15 -p "Select video to watch: ")"
     [[ -z "$video" ]] && break
     [[ ! -d "$video" ]] && [[ ! -f "$video" ]] && continue
-    [[ -d "$video" ]] && [[ "$video" == "tmp" ]] && player="mpv"
+    [[ $(pwd | grep "eighty") ]] && player="vlc"
     [[ -d "$video" ]]; cd "$video" && continue
-    [[ -f "$video" ]]; $player "$video" && break
+    [[ -f "$video" ]]; $player "$video" && $boolOpened=$true && break
 done
 
-input=$(dmenu -p "Delete video after viewing [YES]: ")
-[[ "$input" == "YES" ]] && rm -v "$video"
+[[ $isOpened==$true ]] && input=$(dmenu -p "Delete video after viewing [YES]: ") && [[ "$input" == "YES" ]] && rm -v "$video"
