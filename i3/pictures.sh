b0a0d5174a3c9cbe0c3df7ca5d6afd5a39caf4f8
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-08-06 22:40:52 -0500

    picture script now shows most recent pictures first

--- i3/pictures.sh
+++ i3/pictures.sh
@@ -3,5 +3,5 @@
 # Show list of pictures in set directory and copy image to clipboard
 dir=$HOME/Pictures/lol
 cd "$dir"
-input=$(ls | dmenu -l 15 -p "Select image to copy: ")
+input=$(ls -t | dmenu -l 15 -p "Select image to copy: ")
 xclip -selection clipboard -t image/png -i "$input"
