70edd9702c354116b02ed707c8bc6d15860846d9
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-08-07 22:52:41 -0500

    bookmark script now moves cursor to focused window

--- i3/bookmark.sh
+++ i3/bookmark.sh
@@ -1,5 +1,5 @@
 #1/bin/sh
-
+$HOME/.i3/move-cursor-to-focused.sh
 file=/$HOME/.i3/bookmarks.txt
 bookmark=$(cat $file | dmenu -l 20 -p "Search for a bookmark: " | sed "s/ #.*$//")
 xdotool type $bookmark
