dc2382c74ef570d2e71ba78f8c1c7b4e83f6d34c
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-08-24 00:25:46 +0300

    Added prayer times for month 8, and updated bookmarks script

--- i3/bookmark.sh
+++ i3/bookmark.sh
@@ -3,3 +3,4 @@ $HOME/.i3/move-cursor-to-focused.sh
 file=/$HOME/.i3/bookmarks.txt
 bookmark=$(cat $file | dmenu -l 20 -p "Search for a bookmark: " | sed "s/ #.*$//")
 xdotool type $bookmark
+xdotool key Enter
