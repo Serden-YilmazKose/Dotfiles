5ec6abe60bda27dcaa69a86755e20522938e93d3
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-09-25 20:47:20 +0300

    Improved feh script, now its more on time.

--- i3/feh.sh
+++ i3/feh.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-time_to_60=$((60 - $(date +%S)))
-sleep "$time_to_60"
 while true; do
     feh --bg-scale --no-fehbg -z $HOME/Pictures/feh/*
-    sleep 60
+    time_to_60=$((60 - $(date +%S)))
+    sleep "$time_to_60"
 done
