67d2c1010dac050eba992a5794a275b3d217ab23
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-11-17 10:23:28 +0200

    Added script killer, updated i3 config, and removed dummy script

--- .local/bin/killer.sh
+++ .local/bin/killer.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Go to scripts dir
-cd $HOME/.i3 || exit 1
+cd ~/.local/bin || exit 1
 
-# Get all script names
-# scripts=$(ls $HOME/.i3/*.sh | sed ':a;N;$!ba;s/\n/|/g')
 scripts_array=()
-scripts=$(ls)
+scripts=$(ls *.sh)
+
 for script in *.sh; do
-	[[ -f "$script" ]] && pgrep -f $script && scripts_array+=($script)
+    [[ -f $script ]] && [[ ! $script=="killer.sh" ]] && pgrep $script && scripts_array+=("$script")
 done
 
-selection=$(echo $scripts_array | dmenu -i -l 10 -p "Select script to kill")
-kill $(pgrep $selection) || notify-send "Killer" "$selection was not killed."
+[[ ! -z $scripts_array ]] && selection=$(echo $scripts_array | dmenu -i -l 10 -p " Selection script to kill: ") || exit 1
+kill $(pgrep $selection) || notify-send "Killer" "Unable to kill: $selection"
--- .local/bin/select_script.sh
+++ .local/bin/select_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-selection=$(ls ~/.i3/*.sh | sed "s/^.*\///; s/\.sh//; /select_script/d" | dmenu -l 20 -p "Select script to run: ")
+cd ~/.local/bin || exit 1
+selection=$(ls *.sh | sed "s/^.*\///; s/\.sh//; /select_script/d" | dmenu -l 20 -p "Select script to run: ")
 
 $HOME/.i3/$selection.sh
