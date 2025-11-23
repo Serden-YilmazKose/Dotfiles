c1a0b32e1578a01771a219335dfd1359e5bc3b7a
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-11-24 00:28:59 +0200

    music.sh: change order, so it checks if there is a url in clipboard before opening dmenu

--- .local/bin/music.sh
+++ .local/bin/music.sh
@@ -5,14 +5,14 @@ dir=~/Music/Music
 mkdir -p "$dir"
 cd "$dir"
 
+# Read URL
+url=$(xclip -selection clipboard -o -t text/plain | head -n 1 | xargs)
+[[ -z "$url" ]] && notify-send "yt-dlp" "No URL found in clipboard" && exit 1
+
 # Ask user what directory the song should be downloaded to
 selection=$(ls -d */ | sed "s/\/$//" | dmenu -l 10 -i -p "Music: Choose a destination directory: ")
 [[ ! -d "$selection" ]] && mkdir -p "$selection"
 cd "$selection" || exit 1
-
-# Read URL
-url=$(xclip -selection clipboard -o -t text/plain | head -n 1 | xargs)
-[[ -z "$url" ]] && notify-send "yt-dlp" "No URL found in clipboard" && exit 1
 
 # Notify that download has started
 notify-send "Music" "Downloading $url"
