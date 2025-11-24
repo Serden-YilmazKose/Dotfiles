d4e902effea3850eb42e0f9afd0b4b94cf368dd2
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-11-24 09:20:23 +0200

    music.sh: sanitizes URL if it includes ampersand, to avoid downloading entire playlists

--- .local/bin/music.sh
+++ .local/bin/music.sh
@@ -10,6 +10,9 @@ url=$(xclip -selection clipboard -o -t text/plain | head -n 1 | xargs)
 [[ -z "$url" ]] && notify-send "Music" "Clipboard is empty." && exit 1
 echo "$url" | grep -q "^http[s]?://" && notify-send "Music" "No URL found." && exit 1
 
+# Check if url has an ampersand in it, if so, sanitize it
+echo "$url" | grep -q "&" && url=$($HOME/.local/bin/sanitize_youtube.sh "$url" silent)
+
 # Ask user what directory the song should be downloaded to
 selection=$(ls -d */ | sed "s/\/$//" | dmenu -l 10 -i -p "Music: Choose a destination directory: ")
 [[ ! -d "$selection" ]] && mkdir -p "$selection"
