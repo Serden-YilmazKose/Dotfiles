94d749c5c550f2baf60900dbed4679b8761a358c
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-11-25 13:25:54 +0200

    emoji.sh: Forgot to load it into clipboard.

--- .local/bin/emoji.sh
+++ .local/bin/emoji.sh
@@ -2,7 +2,7 @@
 
 file="$HOME/.local/share/files/emoji"
 selection=$(grep -v "#" "$file" | dmenu -i -l 15 -fn Monospace-24 | awk '{print $1}' | tr -d '\n')
-[[ ! -z "$selection" ]] && cat "$selection" | xclip -selection clipboard || exit 1
+[[ ! -z "$selection" ]] && echo "$selection" | xclip -selection clipboard || exit 1
 # grep -v "#" "$file" | dmenu -i -l 15 -fn Monospace-24 | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
 
 notify-send "$selection now copied to clipboard."
