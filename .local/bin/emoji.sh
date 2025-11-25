3b4fc7f262ca706b8858e190cee592b8b26605a5
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-11-25 13:24:44 +0200

    emoji.sh: Fixed the notify send to show the emoji, and not the content of the clipboard.

--- .local/bin/emoji.sh
+++ .local/bin/emoji.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 file="$HOME/.local/share/files/emoji"
-grep -v "#" "$file" | dmenu -i -l 15 -fn Monospace-24 | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
+selection=$(grep -v "#" "$file" | dmenu -i -l 15 -fn Monospace-24 | awk '{print $1}' | tr -d '\n')
+[[ ! -z "$selection" ]] && cat "$selection" | xclip -selection clipboard || exit 1
+# grep -v "#" "$file" | dmenu -i -l 15 -fn Monospace-24 | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
 
-notify-send "$(xclip -o selection clipboard) now copied to clipboard."
+notify-send "$selection now copied to clipboard."
