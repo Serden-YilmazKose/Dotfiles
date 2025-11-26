b84f22e1b30b6385ecb3f56aed7fe991f4f38609
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-11-26 22:01:31 +0200

    yt2rss.sh: Now does not add a newline, since it is now echo -n

--- .local/bin/yt2rss.sh
+++ .local/bin/yt2rss.sh
@@ -5,4 +5,4 @@ html=$(curl -s "$url")
 channel_id=$(echo "$html" | grep -oP 'channel_id=[a-zA-Z0-9_-]+' | head -n1 | cut -d= -f2)
 # channel_id="${url##*/}"
 channel_name=$(echo "$html" | grep -oP '(?<=<title>).*?(?=</title>)' | sed 's/ - YouTube//' | tr -d '\n')
-echo "https://www.youtube.com/feeds/videos.xml?channel_id=$channel_id youtube # $channel_name" | xclip -selection clipboard && notify-send "Youtube RSS feed copied to clipboard!"
+echo -n "https://www.youtube.com/feeds/videos.xml?channel_id=$channel_id youtube # $channel_name" | xclip -selection clipboard && notify-send "Youtube RSS feed copied to clipboard!"
