4205621fdf4047b604e444fbf129008c2cff4590
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-11-27 23:31:02 +0200

    open_video.sh: using pattern expression for faster results.

--- .local/bin/open_video.sh
+++ .local/bin/open_video.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Open video link based on the ID betwen the square brackets
-id=$(xdotool getwindowfocus getwindowname | cut -d "[" -f2 | cut -d "]" -f1)
+id=$(xdotool getwindowfocus getwindowname)
+id="${id##*\[}"
+id="${id%%\]*}"
 firefox "https://www.youtube.com/watch?v=$id"
