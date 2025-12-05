e8537a6ef7e38b873cdbe709fac5ca3a7b7a935c
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-12-05 13:04:49 +0200

    unread_news.sh: Echo out the number of unread articles on newsboat.

---
+++ .local/bin/unread_news.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get number of unread articles, which is in this format:
+# 20 unread articles
+news=$(newsboat -x print-unread || echo "-1")
+# Use parameter expression to get the first word (number), and echo it out
+news="${news%% *}"
+echo "$news" && pkill -RTMIN+4 i3blocks
