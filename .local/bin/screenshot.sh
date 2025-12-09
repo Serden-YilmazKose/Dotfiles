94db9d8b3c230d3b90b299bb11840b78f44df72e
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-12-09 22:43:11 +0200

    screenshot.sh: Screenshots now ignore cursor.

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -5,17 +5,17 @@
 # Case statement to go through the different style
 case $style in
     file_full)
-        maim --format=png "/home/$USER/Pictures/screenshot-$(date -u +'%Y%m%d-%H%M%SZ')-all.png";;
+        maim --hidecursor --format=png "/home/$USER/Pictures/screenshot-$(date -u +'%Y%m%d-%H%M%SZ')-all.png";;
     file_active)
-        maim --format=png --window $(xdotool getactivewindow) "/home/$USER/Pictures/screenshot-$(date -u +'%Y%m%d-%H%M%SZ')-current.png";;
+        maim --hidecursor --format=png --window $(xdotool getactivewindow) "/home/$USER/Pictures/screenshot-$(date -u +'%Y%m%d-%H%M%SZ')-current.png";;
     file_select)
-        maim --format=png --select "/home/$USER/Pictures/screenshot-$(date -u +'%Y%m%d-%H%M%SZ')-selected.png";;
+        maim --hidecursor --format=png --select "/home/$USER/Pictures/screenshot-$(date -u +'%Y%m%d-%H%M%SZ')-selected.png";;
     clip_full)
-        maim --format=png | xclip -selection clipboard -t image/png;;
+        maim --hidecursor --format=png | xclip -selection clipboard -t image/png;;
     clip_active)
-        maim --format=png --window $(xdotool getactivewindow) | xclip -selection clipboard -t image/png;;
+        maim --hidecursor --format=png --window $(xdotool getactivewindow) | xclip -selection clipboard -t image/png;;
     clip_select)
-        maim --format=png --select | xclip -selection clipboard -t image/png;;
+        maim --hidecursor --format=png --select | xclip -selection clipboard -t image/png;;
     *)
         notify-send "Screenshot" "Failed: No argument specified";;
 esac
