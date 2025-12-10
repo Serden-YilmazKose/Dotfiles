047a0952e955c17be93b776af130cf80e1bf4165
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-12-10 10:12:08 +0200

    Auto commit: .local/bin/update.sh

--- .local/bin/update.sh
+++ .local/bin/update.sh
@@ -7,5 +7,5 @@ updates=$(pacman -Qu)
 [[ -n "$updates" ]] && notify-send -u critical "Package Updates Available" "You have updates available for the following packages:\n$updates" --icon=dialog-information
 
 # Update i3blocks
-package_number=$(echo "$updates" | wc -l)
+package_number=$(echo -n "$updates" | wc -l)
 echo "$package_number"
