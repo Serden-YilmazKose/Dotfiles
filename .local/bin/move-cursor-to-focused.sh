eceb48079cf8cd03ebb0b72eba60c97d0e07becf
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-12-15 22:16:02 +0200

    Auto commit: .local/bin/move-cursor-to-focused.sh

--- .local/bin/move-cursor-to-focused.sh
+++ .local/bin/move-cursor-to-focused.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 focused_window=$(xdotool getwindowfocus -f)
-output=$(xdotool getwindowgeometry --shell $focused_window)
-width=$(echo $output | awk '{print $4}' | cut -d "=" -f 2)
-height=$(echo $output | awk '{print $5}' | cut -d "=" -f 2)
+output=$(xdotool getwindowgeometry --shell "$focused_window")
+width=$(echo "$output" | awk '{print $4}' | cut -d "=" -f 2)
+height=$(echo "$output" | awk '{print $5}' | cut -d "=" -f 2)
 move_y=$((height / 8))
 move_x=$((width / 4))
-xdotool mousemove --window $focused_window $move_x $move_y
+xdotool mousemove --window "$focused_window" $move_x $move_y
