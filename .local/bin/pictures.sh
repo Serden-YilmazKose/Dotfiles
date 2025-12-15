8c0db8280100ec05b53f3d20ac71b8b7abce3282
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-12-15 22:16:02 +0200

    Auto commit: .local/bin/pictures.sh

--- .local/bin/pictures.sh
+++ .local/bin/pictures.sh
@@ -2,6 +2,6 @@
 
 # Show list of pictures in set directory and copy image to clipboard
 dir=$HOME/Pictures/lol
-cd "$dir"
+cd "$dir" || exit 1
 input=$(ls -t | dmenu -l 15 -p "Select image to copy: ")
 xclip -selection clipboard -t image/png -i "$input"
