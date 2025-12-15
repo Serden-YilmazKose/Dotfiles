330cab26cd8dabf968cb4bfc96b535b845a8db8e
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-12-15 22:16:02 +0200

    Auto commit: .local/bin/killer.sh

--- .local/bin/killer.sh
+++ .local/bin/killer.sh
@@ -13,4 +13,4 @@ done
 
 [[ ${#scripts_array[@]} -eq 0 ]] && notify-send "Killer" "No running scripts." && exit 1
 selection=$(printf "%s\n" "${scripts_array[@]}" | dmenu -i -l 10 -p " Selection script to kill: ") || exit 1
-kill $(pgrep -f $selection) && notify-send "Killer" "Killed: $selection" || notify-send "Killer" "Unable to kill: $selecttion"
+{ kill $(pgrep -f "$selection") && notify-send "Killer" "Killed: $selection" ;} || notify-send "Killer" "Unable to kill: $selection"
