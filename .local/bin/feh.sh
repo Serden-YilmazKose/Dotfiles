52ffed5276cc44c5a96d4e7d3e4e4cb8ee43dc91
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-12-15 22:16:02 +0200

    Auto commit: .local/bin/feh.sh

--- .local/bin/feh.sh
+++ .local/bin/feh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do
-    feh --bg-scale --no-fehbg -z $HOME/Pictures/feh/*
+    feh --bg-scale --no-fehbg -z "$HOME"/Pictures/feh/*
     time_to_60=$((60 - $(date +%S)))
     sleep "$time_to_60"
 done
