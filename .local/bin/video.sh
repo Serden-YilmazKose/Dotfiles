eccc9cd1af64713b9e81e15799cdbad655f404da
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2025-12-15 22:16:03 +0200

    Auto commit: .local/bin/video.sh

--- .local/bin/video.sh
+++ .local/bin/video.sh
@@ -4,17 +4,17 @@ video=""
 player="mpv"
 isOpened="false"
 dir=$HOME/Videos
-cd $HOME/Videos/ || exit 1
+cd "$HOME/Videos/" || exit 1
 while [[ ! -f $video ]] ; do
     video="$(ls -tr | dmenu -i -l 15 -p "Select video to watch: ")"
     [[ -z "$video" ]] && exit 1
-    [[ -z $( ls | grep -F "$video" ) ]] && continue
+    ls | grep -qF "$video" || continue
     [[ ! -d "$video" ]] && [[ ! -f "$video" ]] && continue
-    [[ $(pwd | grep "eighty") ]] && player="vlc"
+    pwd | grep -q "eighty"  && player="vlc"
     [[ -d "$video" ]]; cd "$video" && continue
     [[ -f "$video" ]]; $player "$video" && isOpened="true" && break
 done
 
-[[ -z $(pwd | grep -F "/lol" ) ]] && [[ "$isOpened" = "true" ]] && input=$(echo -e "YES\nLOL" | dmenu -i -p "What to do with video [YES/LOL]: ")
+pwd | grep -qF "/lol" || { [[ "$isOpened" = "true" ]] && input=$(echo -e "YES\nLOL" | dmenu -i -p "What to do with video [YES/LOL]: ") ;}
 [[ "$input" == "YES" ]] && rm -v "$video" && notify-send "Video" "$video deleted."
 [[ "$input" == "LOL" ]] && mv "$video" "$dir/lol" && notify-send "Video" "$video moved to lol."
