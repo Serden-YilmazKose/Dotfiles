cc96f23e73c5f514bdb5480f4279ca69e6af67a7
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2026-01-26 15:03:08 +0200

    youtube.sh, save_toread: If youtube.sh fails, save_toread.sh now takes an argument.

--- .local/bin/save_toread.sh
+++ .local/bin/save_toread.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-toread="$(xsel --clipboard --output)"
-file="$HOME/.i3/toread.txt"
- echo "$toread" | grep -q "^http" || { notify-send "Oops." "Not a link!"; exit 1; }
+[ -n "$1" ] && toread="$1" || toread="$(xsel --clipboard --output)"
 
-if grep -Fxq "$toread" "$file"; then
-    notify-send "Oops." "Already toreaded!"
-    exit 0
-fi
+file="$HOME/.i3/toread.txt"
+echo "$toread" | grep -q "^http" || { notify-send "Oops." "Not a link!"; exit 1; }
+
+grep -Fxq "$toread" "$file" &&  notify-send "Oops." "Already toreaded!" && exit 1
 
 notify-send "Toread added!" "$toread is now saved to the file."
 echo "$toread" >> "$file"
--- .local/bin/youtube.sh
+++ .local/bin/youtube.sh
@@ -16,7 +16,7 @@ echo "$url" | grep -q "&" && url=$("$HOME"/.local/bin/sanitize_youtube.sh "$url"
 notify-send "yt-dlp" "Downloading $url."
 
 # Download with mp4 preference (fall back to best if no mp4 found)
-yt-dlp -f 'bv*[ext=mp4]+ba[ext=m4a]/bv*+ba/best' "$url" || { notify-send "yt-dlp" "Download failed." && exit 1 ;}
+yt-dlp -f 'bv*[ext=mp4]+ba[ext=m4a]/bv*+ba/best' "$url" || { "$HOME/.local/bin/save_toread.sh" && exit 1 ;}
 
 # Get video name and notify user
 title=$(ls -t | head -n 1)
