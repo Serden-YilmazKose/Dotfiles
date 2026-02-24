ab243f484c28332ab5ab18e5ee2c4c50ebb42502
Serden-Yilmaz Kose <Serden-Yilmaz.Kose@student.oulu.fi>
2026-02-24 23:10:14 +0200

    lyrics.sh: Added some features

--- .local/bin/lyrics.sh
+++ .local/bin/lyrics.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # If the user wants to, edit the lyrics
 edit_lyrics() {
-    END=$SECONDS
-    ELAPSED=$((END - START))
+    ELAPSED="$1"
+    echo "$ELAPSED"
     [ "$ELAPSED" -lt 5 ] && selection=$(echo -e "No\nYes" | dmenu -p "Would you like to edit the lyrics of: $song")
     [ "$selection" = "Yes" ] && st nvim "$file" && open_lyrics
     exit 0
@@ -10,8 +10,12 @@ edit_lyrics() {
 
 # Compile the lyrics, open using zathura, and prompt for lyrics edit
 open_lyrics() {
+    # Starting time
+    START=$SECONDS
     typst compile "$file" - | zathura -
-    edit_lyrics
+    END=$SECONDS
+    ELAPSED=$((END - START))
+    edit_lyrics "$ELAPSED"
 }
 
 # Create the lyrics, and open them in nvim
@@ -19,9 +23,6 @@ create_lyrics() {
     cp "$lyrics_template" "$file"
     st nvim "$file"
 }
-
-# Starting time
-START=$SECONDS
 
 # Check if needed components exist on the system
 zathura --version   || { notify-send "Lyrics" "Zathura needed for lyrics.sh."   && exit 1;}
